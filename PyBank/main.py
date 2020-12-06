4a8fa9439f4aed5e2d700ec4899e4c0e8064ee1a
JRandle23 <jenny.randle23@gmail.com>
2020-12-06 15:11:08 -0800

    adding code

---
+++ PyBank/main.py
@@ -0,0 +1,25 @@
+print("Financial Analysis")
+print("...............................")
+
+import csv
+
+csvpath = 'PyBank/Resources/budget_data.csv'
+
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+
+    next(csvfile)
+    total = 0
+    for row in csvreader:
+        total += float(row[1])
+    print("Total: {}".format(total))
+
+
+
+
+
+    
+
+
+
+
