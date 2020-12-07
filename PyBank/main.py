1313984111ba854073ea658ac87fe175deb3b110
JRandle23 <jenny.randle23@gmail.com>
2020-12-07 00:26:18 -0800

    uhg

--- PyBank/main.py
+++ PyBank/main.py
@@ -10,17 +10,35 @@ date = []
 profits = []
 change = []
 
+
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     
+    header = next(csvreader)
+
     for row in csvreader:
 
         date.append(row[0])
         profits.append(row[1])
+    
     for total_months in date:
-        print("Total Months: ", len(date) - 1)
+        print("Total Months: ", len(date))
         
         break
+
+    total = 0
+
+    for numbers in range(0, len(profits)):
+        total = total + profits[numbers]
+        print("Total: ", total)
+
+        break
+        
+
+
+
+   
+        
 
     
 
