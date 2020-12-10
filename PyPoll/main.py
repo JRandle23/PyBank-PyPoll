f825c66abfc59ebd65db6204b79e3adad31a856e
JRandle23 <jenny.randle23@gmail.com>
2020-12-09 23:09:36 -0800

    adding code

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,26 +1,30 @@
-
+#import modults
 import os
 import csv
+
 
 print("Election Results")
 print("--------------------------")
 
-
+#open csv file
 csvpath = 'PyPoll/Resources/election_data.csv'
 
 candidates = []
 voter_id = []
 
+#read csc file
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
 
     header = next(csvreader)
 
     for row in csvreader:
-
+        
+        #append lists with column data
         voter_id.append(row[0])
         candidates.append(row[2])
 
+    #calculate total votes
     for totalvotes in voter_id:
         totalvotes = len(voter_id)
 
@@ -29,9 +33,11 @@ with open(csvpath) as csvfile:
 
         break
     
+    #count how many votes each candidate received 
     from collections import Counter
     counted_names = Counter(candidates).items()
     
+    #calculate percentage of vote each candidate had 
     percentages = {x: float(int(y)/len(candidates) * 100) for x, y in counted_names}
     
     
@@ -64,7 +70,7 @@ with open(csvpath) as csvfile:
     output_file = os.path.join('PyPoll/Analysis/output_text.txt')
 
     with open(output_file, 'w') as txtfile:
-        txtfile.write(output)
+        txtfile.write(repr(formatted))
 
     
     
